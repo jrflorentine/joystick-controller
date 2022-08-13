5157f186963e072d0890433027113018e59bd8f1
jrflorentine <53338319+jrflorentine@users.noreply.github.com>
2022-08-13 20:49:29 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,42 +1,44 @@
 pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
 radio.setGroup(1)
 basic.forever(function () {
-    while (pins.digitalReadPin(DigitalPin.P1) == 1) {
-        if (pins.analogReadPin(AnalogPin.P0) < 600) {
-            basic.showLeds(`
-                . . # . .
-                . # . . .
-                # # # # #
-                . # . . .
-                . . # . .
-                `)
-        } else if (pins.analogReadPin(AnalogPin.P0) > 800) {
-            basic.showLeds(`
-                . . # . .
-                . . . # .
-                # # # # #
-                . . . # .
-                . . # . .
-                `)
-        } else if (pins.analogReadPin(AnalogPin.P2) > 800) {
-            basic.showLeds(`
-                . . # . .
-                . # # # .
-                # . # . #
-                . . # . .
-                . . # . .
-                `)
-        } else if (pins.analogReadPin(AnalogPin.P2) < 600) {
-            basic.showLeds(`
-                . . # . .
-                . . # . .
-                # . # . #
-                . # # # .
-                . . # . .
-                `)
-        } else {
-            basic.clearScreen()
-        }
+    if (pins.analogReadPin(AnalogPin.P2) < 500) {
+        radio.sendString("left")
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (pins.analogReadPin(AnalogPin.P2) > 750 && pins.analogReadPin(AnalogPin.P2) < 850) {
+        radio.sendString("right")
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (pins.analogReadPin(AnalogPin.P0) > 600) {
+        radio.sendString("up")
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (pins.analogReadPin(AnalogPin.P0) < 300) {
+        radio.sendString("down")
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else {
+        radio.sendString("stop")
+        basic.clearScreen()
     }
-    basic.showIcon(IconNames.No)
 })
